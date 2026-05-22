01756ff9dd35253c2937b2407d469fdf7abe6b65
Pop_os! <danielremensperger22@gmail.com>
2026-05-22 12:28:13 +0200

    PSR-4 fixes

--- app/View/Components/DisciplineForm.php
+++ app/View/Components/DisciplineForm.php
@@ -6,21 +6,15 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class teamtable-form extends Component
+class DisciplineForm extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
-        return view('components.teamtable-form');
+        return view('components.discipline-form');
     }
 }
--- app/View/Components/KlasseForm.php
+++ app/View/Components/KlasseForm.php
@@ -6,21 +6,15 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class disciplineform extends Component
+class KlasseForm extends Component
 {
-    /**
-     * Create a new component instance.
-     */
     public function __construct()
     {
         //
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     */
     public function render(): View|Closure|string
     {
-        return view('components.discipline-form');
+        return view('components.klasse-form');
     }
 }
